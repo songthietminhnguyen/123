cbb3950a5f7a2f43a8af6816257111c5952e35bd
songthietminhnguyen <songthietminhnguyen@gmail.com>
2024-10-10 17:49:39 -0700

    input for k

--- combination/combination_example.cpp
+++ combination/combination_example.cpp
@@ -20,7 +20,7 @@ uint16_t factorial(const uint16_t x) {
  * Main function to calculate C(n,k);
  */
 int main() {
-    int n;
+    int n, k;
 
     // get and validate user input for n
     cout << "Enter n: ";
@@ -28,7 +28,15 @@ int main() {
     while (n < 0) {
         cout << "Invalid input. enter a number greater than or equalto 0:";
         cin >> n;
+    }
 
+    // get and validate user input for k
+    cout << "Enter k: ";
+    cin >> k;
+    while (k < 0 || k > n) {
+        cout << "Invalid input. Enter a number between 0 and " << n << "; ";
+        cin >> k;
+    }
     // calculate C(n,k) = n! / (k! * (n-k)!)
     uint16_t c_n_k = factorial(n);
 
